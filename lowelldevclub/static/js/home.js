45586dd58e8c194878a98851a80aa7660750f863
calee14 <caplee1000@gmail.com>
2019-02-12 21:19:31 -0800

    changing white color to match logo

--- lowelldevclub/static/js/home.js
+++ lowelldevclub/static/js/home.js
@@ -8,7 +8,7 @@ particlesJS("particles-js", {
       }
     },
     "color": {
-      "value": "#ffffff"
+      "value": "#EFEFEF"
     },
     "shape": {
       "type": "circle",
@@ -48,7 +48,7 @@ particlesJS("particles-js", {
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#ffffff",
+      "color": "#EFEFEF",
       "opacity": 0.4,
       "width": 1
     },
