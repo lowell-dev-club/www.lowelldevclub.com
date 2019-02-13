061363b41fa0a50e12d47b342cd9bdf30937ce48
calee14 <caplee1000@gmail.com>
2019-02-12 21:10:07 -0800

    rendering home page

--- lowelldevclub/views.py
+++ lowelldevclub/views.py
@@ -2,5 +2,5 @@ from lowelldevclub import app
 from flask import render_template, request, make_response, redirect
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return 'Lowell Dev Club'
+def home():
+    return render_template('home.html')
