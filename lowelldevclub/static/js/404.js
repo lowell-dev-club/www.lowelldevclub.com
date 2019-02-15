84c6fe2e304b9b6ee4e00e9b79eb7a6375ffaeb9
Savagecoder77 <savagecoder77@gmail.com>
2019-02-15 13:01:08 -0800

    new 404 page

---
+++ lowelldevclub/static/js/404.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+  var canvas = document.getElementById("particles-js");
+  canvas.width = $("#parent").width();
+  canvas.height = $("#parent").height();
+
+	var wind =new buzz.sound("http://k003.kiwi6.com/hotlink/bcj5gqrt0n/Wind-Mark_DiAngelo-1940285615.mp3");
+	wind.loop();
+	wind.play();
+});
+
+
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 180,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "random"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#edfedf"
+      },
+      "polygon": {
+        "nb_sides": 0
+      },
+
+    },
+    "opacity": {
+      "value": 0.9,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 10,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "bubble",
+
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 0
+        }
+      },
+      "bubble": {
+        "distance": 200,
+        "size": 12,
+        "duration": 0.5,
+        "opacity": 1,
+        "speed": 10
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+					 );
