1051e7ac4197cb56a36a5bcf081dfb115755a32c
calee14 <caplee1000@gmail.com>
2019-02-14 22:40:30 -0800

    adding routes for navbar

--- lowelldevclub/views.py
+++ lowelldevclub/views.py
@@ -4,3 +4,15 @@ from flask import render_template, request, make_response, redirect
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/schedule', methods=['GET'])
+def schedule():
+    return render_template('schedule.html')
+
+@app.route('/meetings', methods=['GET'])
+def meetings():
+    return render_template('meetings.html')
