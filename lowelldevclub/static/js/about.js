5bff6fea78eea8e462c9db2eb0e589e8e5839bfc
Savagecoder77 <savagecoder77@gmail.com>
2019-02-23 17:38:14 -0800

    simplify typed.js

--- lowelldevclub/static/js/about.js
+++ lowelldevclub/static/js/about.js
@@ -1,39 +1,7 @@
-import Typed from 'https://cdn.jsdelivr.net/npm/typed.js@2.0.10/src/typed.js'
-
 //https://mattboldt.com/ creator of typed.js
 
-$(function(){
-	$(".typed").typed({
-		strings: ["Developers.", "Designers.", "Lowell Students.", "Computer Scientists"],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 30,
-		// time before typing starts
-		startDelay: 1200,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 500,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: 5,
-		// show cursor
-		showCursor: false,
-		// character for cursor
-		cursorChar: "|",
-		// attribute to type (null == text)
-		attr: null,
-		// either html or text
-		contentType: 'html',
-		// call when done callback function
-		callback: function() {},
-		// starting callback function before each string
-		preStringTyped: function() {},
-		//callback for every typed string
-		onStringTyped: function() {},
-		// callback for reset
-		resetCallback: function() {}
-	});
-});
+var typed = new Typed('#typed',{
+                    stringsElement: '#typed-strings',
+                    backSpeed: 40,
+                    typeSpeed: 40
+                  });
