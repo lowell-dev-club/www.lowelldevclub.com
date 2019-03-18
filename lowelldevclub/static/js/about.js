b741ace28eba89f78ce6f3ef0d87eac34cbfcc9d
calee14 <caplee1000@gmail.com>
2019-03-17 18:08:08 -0700

    adding the messages for the typed js

--- lowelldevclub/static/js/about.js
+++ lowelldevclub/static/js/about.js
@@ -38,12 +38,12 @@ $(function(){
 		// callback for reset
 		resetCallback: function() {}
 	});
-	$(".meet_leaders").typed({
-		strings: ["Meet the leaders:"],
+	$(".about_paragraph").typed({
+		strings: ["Lowell Dev Club is a club for everyone!"],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
 		// typing speed
-		typeSpeed: 30,
+		typeSpeed: 10,
 		// time before typing starts
 		startDelay: 1200,
 		// backspacing speed
@@ -71,5 +71,39 @@ $(function(){
 		// callback for reset
 		resetCallback: function() {}
 	});
+	$(".meet_leaders").typed({
+		strings: ["Meet the leaders:"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 2400,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: false,
+		// false = infinite
+		loopCount: false,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+
 });
 
