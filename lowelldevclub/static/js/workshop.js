71ad4dc2662e022fab43248753cd74e83678c140
calee14 <caplee1000@gmail.com>
2019-09-18 21:26:36 -0700

    fixed links

--- lowelldevclub/static/js/workshop.js
+++ lowelldevclub/static/js/workshop.js
@@ -90,19 +90,19 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
-var i = 0;
+var i1 = 0;
 var txt1 = "1. Repl.it";
 var speed = 40;
 var delayInMilliseconds = 1200
-setTimeout(function() {if (i < txt1.length) {document.getElementById("link1").innerHTML += txt1.charAt(i);i++;setTimeout(typeWriter1, speed);}}, delayInMilliseconds);
-function typeWriter1() {if (i < txt1.length) {document.getElementById("link1").innerHTML += txt1.charAt(i);i++;setTimeout(typeWriter1, speed);}}
-i = 0;
+setTimeout(function() {if (i1 < txt1.length) {document.getElementById("link1").innerHTML += txt1.charAt(i1);i1++;setTimeout(typeWriter1, speed);}}, delayInMilliseconds);
+function typeWriter1() {if (i1 < txt1.length) {document.getElementById("link1").innerHTML += txt1.charAt(i1);i1++;setTimeout(typeWriter1, speed);}}
+var i2 = 0;
 var delayInMilliseconds2 = 3000
 var txt2 = "2. https://lowelldev.club/workshop/hack1";
-setTimeout(function() {if (i < txt2.length) {document.getElementById("link2").innerHTML += txt2.charAt(i);i++;setTimeout(typeWriter2, speed);}}, delayInMilliseconds2);
-function typeWriter2() {if (i < txt2.length) {document.getElementById("link2").innerHTML += txt2.charAt(i);i++;setTimeout(typeWriter2, speed);}}
-i = 0;
-var delayInMilliseconds1 = 5000
+setTimeout(function() {if (i2 < txt2.length) {document.getElementById("link2").innerHTML += txt2.charAt(i2);i2++;setTimeout(typeWriter2, speed);}}, delayInMilliseconds2);
+function typeWriter2() {if (i2 < txt2.length) {document.getElementById("link2").innerHTML += txt2.charAt(i2);i2++;setTimeout(typeWriter2, speed);}}
+var i3 = 0;
+var delayInMilliseconds3 = 5000
 var txt3 = "3. https://lowelldev.club/workshop/hack2";
-setTimeout(function() {if (i < txt3.length) {document.getElementById("link3").innerHTML += txt3.charAt(i);i++;setTimeout(typeWriter3, speed);}}, delayInMilliseconds3);
-function typeWriter3() {if (i < txt3.length) {document.getElementById("link3").innerHTML += txt3.charAt(i);i++;setTimeout(typeWriter3, speed);}}
+setTimeout(function() {if (i3 < txt3.length) {document.getElementById("link3").innerHTML += txt3.charAt(i3);i3++;setTimeout(typeWriter3, speed);}}, delayInMilliseconds3);
+function typeWriter3() {if (i3 < txt3.length) {document.getElementById("link3").innerHTML += txt3.charAt(i3);i3++;setTimeout(typeWriter3, speed);}}
