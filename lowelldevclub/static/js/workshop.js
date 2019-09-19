2fc332730a6a7818e03e745cef009bf889f8b744
Savagecoder77 <savagecoder77@gmail.com>
2019-09-19 10:49:04 -0700

    minimize and fix up js

--- lowelldevclub/static/js/workshop.js
+++ lowelldevclub/static/js/workshop.js
@@ -33,44 +33,12 @@ $(function(){
 		resetCallback: function() {}
 	});
 });
-$(function(){
-	$(".flask").typed({
-		strings: ["Full Stack Web Devlopment"],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 0,
-		// time before typing starts
-		startDelay: 0,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 500,
-		// loop
-		loop: false,
-		// false = infinite
-		loopCount: false,
-		// show cursor
-		showCursor: false,
-		// character for cursor
-		cursorChar: "|",
-		// attribute to type (null == text)
-		attr: null,
-		// either html or text
-		contentType: 'html',
-		// call when done callback function
-		callback: function() {},
-		// starting callback function before each string
-		preStringTyped: function() {},
-		//callback for every typed string
-		onStringTyped: function() {},
-		// callback for reset
-		resetCallback: function() {}
-	});
-});
+$(function(){$(".flask").typed({strings: ["Full Stack Web Devlopment"],stringsElement: null,typeSpeed: 0,startDelay: 0,backSpeed: 20,backDelay: 500,loop: false,loopCount: false,showCursor: false,cursorChar: "|",attr: null,contentType: 'html',callback: function() {},preStringTyped: function() {},onStringTyped: function() {},resetCallback: function() {}});});
+
 /*
 	This is the type script for the club description in the about page
 */
+
 var i = 0;
 var txt = "We are working on the workshop. Check out our github to take a peak at the source code or what this weeks workshop will be on here: https://github.com/lowell-dev-club";
 var speed = 40;
@@ -92,7 +60,6 @@ function typeWriter() {
 }
 var i1 = 0;
 var txt1 = "1. Repl.it";
-var speed = 40;
 var delayInMilliseconds = 1200
 setTimeout(function() {if (i1 < txt1.length) {document.getElementById("link1").innerHTML += txt1.charAt(i1);i1++;setTimeout(typeWriter1, speed);}}, delayInMilliseconds);
 function typeWriter1() {if (i1 < txt1.length) {document.getElementById("link1").innerHTML += txt1.charAt(i1);i1++;setTimeout(typeWriter1, speed);}}
