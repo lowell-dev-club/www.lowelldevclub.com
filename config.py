6b766017d2b4d1dae333cdf1d8ee014e73521f60
RafaelCenzano <savagecoder77@gmail.com>
2020-07-04 15:10:23 -0700

    new config

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+
+'''
+Config class
+'''
+
+
+class Config(object):
+
+    SECRET_KEY = os.environ.get('devclubkey')
+
+    TESTING = False
+    DEBUG = False
+
+    # Using Postgresql
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
