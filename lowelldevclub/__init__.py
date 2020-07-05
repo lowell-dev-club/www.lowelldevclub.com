f35397c15445e8e8413209e25ddfc2ba77635d67
RafaelCenzano <savagecoder77@gmail.com>
2020-07-04 23:34:19 -0700

    new code for new updates to site

--- lowelldevclub/__init__.py
+++ lowelldevclub/__init__.py
@@ -5,11 +5,28 @@ Imports
 # Flask imports
 from flask import Flask
 
+# Import Flask modules
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
 # Create Flask app
 app = Flask(__name__)
+app.config.from_object(Config)
 
-# Add Configurations to app
-app.config.from_pyfile('config.py', silent=True)
+# Create Database object from brcrypt object
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+# Create Login manager
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'warning'
+
+# importing all the models and initializing them
+from lowelldevclub.models import *
+db.create_all()
 
 # Import all views
 import lowelldevclub.views
